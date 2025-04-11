3b13f89b431f0f768d28b536f9fb4ee1ba324067
realdtn <132553203+realdtn2@users.noreply.github.com>
2025-04-11 23:19:53 +0700

    Update show-olm-answers.user.js

--- show-olm-answers.user.js
+++ show-olm-answers.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Show OLM Answers
 // @namespace    http://tampermonkey.net/
-// @version      1.3
+// @version      1.4
 // @description  Show answers that the website leaks
 // @author       realdtn
 // @match        *://*.olm.vn/*
