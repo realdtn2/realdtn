b54b1fa77efc42767d8ee827a9eac0c4d58761e9
realdtn <132553203+realdtn2@users.noreply.github.com>
2025-09-23 12:13:40 +0700

    Create olm-shuffling-disabler.js

---
+++ olm-shuffling-disabler.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         OLM Shuffling Disabler
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Disable question shuffling by overriding _shuffle function
+// @author       realdtn
+// @match        https://olm.vn/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Override the global _shuffle function to prevent shuffling
+    window._shuffle = function(array) {
+        console.log('Shuffling disabled - returning array as-is');
+        return array; // Return the original array without shuffling
+    };
+
+    // Also ensure not_shuffle is set to true
+    const originalInit = window.EXAM_UI?.init;
+    if (originalInit) {
+        window.EXAM_UI.init = function(config) {
+            // Force not_shuffle to be true
+            config.not_shuffle = 1;
+            return originalInit.call(this, config);
+        };
+    }
+
+    console.log('Question shuffling has been disabled');
+})();
