f62d6903a82645923236300bb173413dd410d9f5
realdtn <132553203+realdtn2@users.noreply.github.com>
2026-01-09 20:23:59 +0700

    Add files via upload

---
+++ olm-answers-sniffers.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         OLM Answers Sniffer
+// @namespace    http://tampermonkey.net/
+// @version      3.3.0
+// @description  v3.3 - Revamped
+// @author       realdtn
+// @match        *://*.olm.vn/*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
